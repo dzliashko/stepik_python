4a3c8d8c6bb762b78c1bd8dfab075091174e3f07
Dzmitry Liashko <dzliashko@gmail.com>
2025-12-03 16:56:38 +0300

    Add: temp

--- temp.py
+++ temp.py
@@ -3,12 +3,12 @@ num = int(input())
 
 def rev_last_five(num):
     result = 0
-    cnt = 10
+    cnt = 10000
     while num > 0:
-        result += num % 10 * 10
+        result += num % 10 * cnt
         num = num // 10
-        cnt *= 10
-    return result
+        cnt /= 10
+    return int(result)
 
 
 res = rev_last_five(num)
